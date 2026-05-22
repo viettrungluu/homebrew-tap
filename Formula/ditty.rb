79082ba3a25d394e97dc25f4dd601a47ebad4f2f
Viet-Trung Luu <viettrungluu@gmail.com>
2026-05-22 05:40:55 +0000

    Update ditty to v0.5.2

--- Formula/ditty.rb
+++ Formula/ditty.rb
@@ -1,30 +1,30 @@
 class Ditty < Formula
   desc "Convert line-interactive programs (REPLs, debuggers) into CLI programs"
   homepage "https://github.com/viettrungluu/ditty"
-  version "0.5.1"
+  version "0.5.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.1/ditty-darwin-arm64.tar.gz"
-      sha256 "68e454da4b7c7028b50faa42ef0f865fa804800486c447e12840beecd5ac1390"
+      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.2/ditty-darwin-arm64.tar.gz"
+      sha256 "c8d235edb72f5a8e0a74866dc34ab5717e9e4bf88af116c7814a4da900884ddf"
     end
 
     on_intel do
-      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.1/ditty-darwin-amd64.tar.gz"
-      sha256 "78099f38ff03a4c39e8464490512865bbd7791b8053a5ff06b33ed23fbc5041b"
+      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.2/ditty-darwin-amd64.tar.gz"
+      sha256 "a66c51451dcdd69230c2337a3f254b3e2712c1cf0d2a974e8ea74fcb7c7e2d02"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.1/ditty-linux-arm64.tar.gz"
-      sha256 "9c1d50fa4e96c8e0171b93df8e102aa4bba9d6447e1215f986d57d5fa3aef303"
+      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.2/ditty-linux-arm64.tar.gz"
+      sha256 "aed08a165abd70370fa9034069f6139a2bf30772ca4065df10d5b08cb4a3524f"
     end
 
     on_intel do
-      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.1/ditty-linux-amd64.tar.gz"
-      sha256 "d678c9dcf50a85d1e2470959f838c7fd5f620042f4f4ebba2a60555d355650ea"
+      url "https://github.com/viettrungluu/ditty/releases/download/v0.5.2/ditty-linux-amd64.tar.gz"
+      sha256 "941f9f8d1dfe2334cfd7175540bf7b81f686e3a020131009957774ff32f5ca42"
     end
   end
 
